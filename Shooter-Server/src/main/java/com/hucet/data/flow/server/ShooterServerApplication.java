ff78bb5eae29e31ea4183b43046e9906781cd93e
taesu <kimtaesoo188@gmail.com>
2017-03-11 04:58:52 +0900

    Initial commit

---
+++ Shooter-Server/src/main/java/com/hucet/data/flow/server/ShooterServerApplication.java
@@ -0,0 +1,12 @@
+package com.hucet.data.flow.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShooterServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShooterServerApplication.class, args);
+	}
+}
---
+++ src/main/java/com/hucet/data/flow/ShooterDataFlowApplication.java
@@ -0,0 +1,12 @@
+package com.hucet.data.flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShooterDataFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShooterDataFlowApplication.class, args);
+	}
+}
